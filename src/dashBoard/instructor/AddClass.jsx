30c173c6d18913e128f7834220d2ac6e276cbfbf
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-06-10 00:58:19 +0600

    class data loaded

--- src/dashBoard/instructor/AddClass.jsx
+++ src/dashBoard/instructor/AddClass.jsx
@@ -1,13 +1,52 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../provider/AuthProvider";
+
+const photo_hosting_token = import.meta.env.VITE_Photo_hosting_token;
 
 const AddClass = () => {
+  const { user } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    data.price = Number(data.price);
+    data.seats = Number(data.seats);
+    data = { ...data, status: "pending" };
+
+    // getting error when try to upload an img on imgbb get 400 ,"Empty upload source."
+
+    // const formData = new FormData();
+    // formData.append("classPhoto", data.classPhoto[0]);
+    // console.log(formData);
+
+    // fetch(`https://api.imgbb.com/1/upload?key=${photo_hosting_token}`, {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((res) => res.json())
+    //   .then((photoRes) => {
+    //     console.log(photoRes);
+    //   });
+
+    fetch("http://localhost:5000/addClass", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire("Class Added Successful!", "", "success");
+        }
+      });
+  };
 
   return (
     <div className="hero min-h-screen py-16 bg-[#2F2F2F]">
@@ -30,10 +69,11 @@ const AddClass = () => {
               <span className="label-text text-white text-sm">Class Image</span>
             </label>
             <input
-              type="file"
-              className="file-input file-input-bordered file-input-info"
+              type="url"
+              className="input input-bordered"
+              // className="file-input file-input-bordered file-input-info"
               placeholder="Enter your class image"
-              {...register("example", { required: true })}
+              {...register("classPhoto", { required: true })}
             />
             <label className="label">
               <span className="label-text text-white text-sm">
@@ -43,6 +83,8 @@ const AddClass = () => {
             <input
               className="input input-bordered"
               placeholder="Enter your email"
+              defaultValue={user?.displayName}
+              readOnly
               type="text"
               {...register("instructorName", { required: true })}
             />
@@ -54,6 +96,8 @@ const AddClass = () => {
             <input
               className="input input-bordered"
               placeholder="Enter your password"
+              defaultValue={user?.email}
+              readOnly
               type="email"
               {...register("instructorEmail", { required: true })}
             />
--- src/pages/AllClasses/Classes.jsx
+++ src/pages/AllClasses/Classes.jsx
@@ -1,41 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+
 const Classes = () => {
+  const { data: allClass = [], refetch } = useQuery(["allClass"], async () => {
+    const res = await fetch("http://localhost:5000/classes");
+    return res.json();
+  });
+
   return (
     <div className="instructor-bg py-8">
       <div className="w-11/12 mx-auto  pt-14">
-        <h1 className="my-4 text-4xl text-center text-white">All Classes</h1>
-        <div className="grid lg:grid-cols-4 gap-3">
-          {/* card 1 */}
-          <div className="card-compact  bg-base-100">
-            <figure>
-              <img
-                src="https://i.ibb.co/R64JZ2R/Untitled-2-01.png"
-                alt="dance"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Show Details</button>
+        <h1 className="my-4 text-4xl text-center text-white">
+          All Classes{allClass.length}
+        </h1>
+        <div className="grid lg:grid-cols-3 gap-3">
+          {/* show all class approved by admin */}
+          {allClass.map((cla) => (
+            <>
+              <div className="card-compact  bg-base-100">
+                <figure>
+                  <img className="" src={cla.classPhoto} alt="dance" />
+                </figure>
+                <div className="card-body ">
+                  <h2 className="text-center text-3xl font-bold">
+                    {cla?.className}
+                  </h2>
+                  <div className="card-actions items-center justify-between">
+                    <p className="text-3xl">Price: ${cla?.price}</p>
+                    <p className="text-3xl">Available Seats: {cla?.seats}</p>
+                    <button className="btn btn-primary btn-sm">
+                      Enroll Class
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          ))}
+
           {/* card 1 */}
-          <div className="card-compact  bg-base-100">
-            <figure>
-              <img
-                src="https://i.ibb.co/R64JZ2R/Untitled-2-01.png"
-                alt="dance"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Show Details</button>
-              </div>
-            </div>
-          </div>
+
           {/* card end */}
         </div>
       </div>
